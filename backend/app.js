0978efc0dae49e0fcc4a8e3284d8680fd1304bab
YassineElkefi <yassine.elkefi6@gmail.com>
2024-02-20 11:43:59 +0100

    Project initialization

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const app = express()
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send("<h1>Backend Works !</h1>")
+})
+app.listen(5000,()=>{
+    console.log("Listening on port 5000");
+})
