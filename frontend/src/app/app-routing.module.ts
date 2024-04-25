828a6fb5629761782c63f59319480f832e388b52
Hiba-Bahri <hibabahri447@gmail.com>
2024-04-25 14:23:19 +0100

    HomePage

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DeliveryComponent } from './delivery/delivery.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path:'Home', component:HomeComponent},
+    {path:'Delivery', component:DeliveryComponent},
+    {path:'About-us', component:AboutUsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,23 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
-import { AppRoutingModule } from './app-routing.module';
+import { CarouselModule } from 'primeng/carousel';
 import { AppComponent } from './app.component';
 import { RequestsComponent } from './requests/requests.component';
 import { OffersComponent } from './offers/offers.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DeliveryComponent } from './delivery/delivery.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ButtonModule } from 'primeng/button';
+import { FooterComponent } from './footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     RequestsComponent,
-    OffersComponent
+    OffersComponent,
+    HomeComponent,
+    NavbarComponent,
+    DeliveryComponent,
+    AboutUsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    CarouselModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
