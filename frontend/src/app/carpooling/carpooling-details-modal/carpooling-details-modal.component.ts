c1e9dadd45faa56406748bf9903a8808abfcf8a3
YassineElkefi <yassine.elkefi6@gmail.com>
2024-05-02 15:08:38 +0100

    Errors resolution

--- frontend/src/app/carpooling/carpooling-details-modal/carpooling-details-modal.component.ts
+++ frontend/src/app/carpooling/carpooling-details-modal/carpooling-details-modal.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { User } from '../../models/User';
 import { Router } from '@angular/router';
 import { Offer } from '../../models/Offer';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-carpooling-details-modal',
@@ -10,17 +11,22 @@ import { Offer } from '../../models/Offer';
 })
 export class CarpoolingDetailsModalComponent {
   isOpen: boolean = false;
-  @Input() user?: User;
+  user?: User;
   @Input() selectedOffer?: Offer;
   @Output() request = new EventEmitter();
   @Output() OfferToBeDeletedToHome = new EventEmitter();
 
-constructor(private router: Router){}
+constructor(private router: Router,private authService: AuthService){}
+
+ngOnInit(): void {
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+  this.user = this.authService.getUser();
+}
 
   openModal() {
     this.isOpen = true;
-    console.log(this.selectedOffer);
-    
+    console.log(this.selectedOffer); 
   }
 
   closeModal() {
