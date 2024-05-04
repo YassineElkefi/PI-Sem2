b60d586af3c29b226bb1a8b7c0063908782c49ab
Hiba-Bahri <hibabahri447@gmail.com>
2024-05-04 17:21:29 +0100

    Errors resolution

--- frontend/src/app/delivery/delivery-send-request/delivery-send-request.component.ts
+++ frontend/src/app/delivery/delivery-send-request/delivery-send-request.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Offer } from '../../models/Offer';
 import { User } from '../../models/User';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-delivery-send-request',
@@ -8,15 +9,19 @@ import { User } from '../../models/User';
   styleUrl: './delivery-send-request.component.css'
 })
 export class DeliverySendRequestComponent {
+
   isOpen = false;
   arrival = '';
   departure = '';
-  @Input() user?: User;
+  locationOptions: string[] = ['Yasminet', 'Mourouj', 'Tekup'];
+  selectedPickup = "";
+  selectedArrival="";
   @Input() selectedOffer?: Offer;
   @Output() requestToHome = new EventEmitter();
+  @Input() user: User;
 
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   openModal() {
     this.isOpen = true;
@@ -27,12 +32,16 @@ export class DeliverySendRequestComponent {
   }
 
   sendRequest() {
+    if (!this.selectedOffer) {
+      console.error('Selected offer is undefined!');
+      return;
+    }
     const requestData = {
-      arrival: this.arrival,
-      departure: this.departure,
+      arrival: this.selectedArrival,
+      departure: this.selectedPickup,
       offer: this.selectedOffer,
       state:'Pending',
-      sender:this.user,
+      sender:this.authService.getUser() ,
       receiver:this.selectedOffer.offeror,
     }
 
