4bf9f2d8aa8fb6f4ee146e3f84566e32edb75fb9
YassineElkefi <yassine.elkefi6@gmail.com>
2024-05-09 15:58:22 +0100

    Auth Security fix

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { CarpoolingHomeComponent } from './carpooling/carpooling-home/carpooling-home.component';
 import { CarpoolingFiltersComponent } from './carpooling/carpooling-filters/carpooling-filters.component';
-import { authGuard } from './auth.guard';
+import { authGuard, authGuard2 } from './auth.guard';
 import { DeliveryHomeComponent } from './delivery/delivery-home/delivery-home.component';
 import { ProfileManagementComponent } from './profile-management/profile-management.component';
 import { OffersManagementComponent } from './offers-management/offers-management.component';
@@ -12,7 +12,7 @@ import { OffersManagementComponent } from './offers-management/offers-management
 const routes: Routes = [
   {path: 'carpooling', component: CarpoolingHomeComponent,},
   {path: 'profile/:_id', component: ProfileManagementComponent, canActivate: [authGuard]},
-  {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
+  {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule), canActivate: [authGuard2]},
   {path:'', component:HomeComponent},
   {path:'delivery', component:DeliveryHomeComponent},
   {path:'about-us', component:AboutUsComponent},
--- frontend/src/app/auth.guard.ts
+++ frontend/src/app/auth.guard.ts
@@ -2,6 +2,17 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
 
+export const authGuard2: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const authService = inject(AuthService);
+  if (authService.isAuthenticated()) {
+    router.navigate(['/']);
+    return false;
+  } else {
+    return true;
+  }
+}
+
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const authService = inject(AuthService);
