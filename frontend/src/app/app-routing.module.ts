363e5cc0fbdf3f5c79b0460b95863ec9644ce98f
YassineElkefi <yassine.elkefi6@gmail.com>
2024-05-15 23:33:11 +0100

    Routing fix

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { CarpoolingHomeComponent } from './carpooling/carpooling-home/carpooling-home.component';
-import { authGuard, authGuard2 } from './auth.guard';
+import { authGuard, authGuard2, authGuard3 } from './auth.guard';
 import { DeliveryHomeComponent } from './delivery/delivery-home/delivery-home.component';
 import { ProfileManagementComponent } from './profile-management/profile-management.component';
 import { OffersManagementComponent } from './offers-management/offers-management.component';
@@ -11,7 +11,7 @@ import { OffersManagementComponent } from './offers-management/offers-management
 const routes: Routes = [
   {path: 'carpooling', component: CarpoolingHomeComponent,},
   {path: 'profile/:_id', component: ProfileManagementComponent, canActivate: [authGuard]},
-  {path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)},
+  {path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule), canActivate:[authGuard3]},
   {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule), canActivate: [authGuard2]},
   {path:  '', component:HomeComponent},
   {path:  'delivery', component:DeliveryHomeComponent},
--- frontend/src/app/auth.guard.ts
+++ frontend/src/app/auth.guard.ts
@@ -18,7 +18,33 @@ export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
 
   if (authService.isAuthenticated()){
+    const userDataString = authService.getUser()
+    if (userDataString){
+      if (JSON.parse(userDataString).cin ="00000000"){
+        router.navigate(["/admin/admin-dashboard"]);
+        return false;
+      }
+    }
     return true;
+  }else{
+    router.navigate(['/auth/login']);
+    return false;
+  }
+}
+
+export const authGuard3: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const authService = inject(AuthService);
+
+  if (authService.isAuthenticated()){
+    const userDataString = authService.getUser()
+    if (userDataString){
+      if (JSON.parse(userDataString).cin ="00000000"){
+        return true;
+      }
+    }
+    router.navigate(['/']);
+    return false;
   }else{
     router.navigate(['/auth/login']);
     return false;
